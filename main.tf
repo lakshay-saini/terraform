3f0db5e3717d60d1f4ed95f05176c354820281e7
lakshay-saini <39418344+lakshay-saini@users.noreply.github.com>
2020-01-19 15:13:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  version = "=1.38.0"
+
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
+}
 resource "azurerm_log_analytics_workspace" "main" {
   name                = "${var.prefix}-log-analytics-workspace"
   location            = "${var.location}"
