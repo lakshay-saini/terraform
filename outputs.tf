d3f2ae39c3073e304703a0291058b921c48731cb
lakshay-saini <39418344+lakshay-saini@users.noreply.github.com>
2020-01-23 23:25:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "id" {
-    value = "${azurerm_log_analytics_workspace.main.id}"
+    value = "${azurerm_log_analytics_workspace.deploy_log_analytics_workspace.id}"
 }
 
 output "name" {
-  value = "${azurerm_log_analytics_workspace.main.name}"
+  value = "${azurerm_log_analytics_workspace.deploy_log_analytics_workspace.name}"
 }
